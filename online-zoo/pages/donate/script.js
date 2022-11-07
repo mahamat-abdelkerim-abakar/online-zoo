e2e135391bdd07e45fa771a10df8738f98ad63ac
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2022-11-07 15:02:31 +0530

    Delete script.js

--- online-zoo/pages/donate/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('logo-anchor').onclick = function () {
-    location.href = '../main/index.html';
-  };
-  document.getElementById('logo-anchor2').onclick = function () {
-    location.href = '../main/index.html';
-  };
