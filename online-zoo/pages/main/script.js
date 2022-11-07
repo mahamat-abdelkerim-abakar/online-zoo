2071a49b8d2df0c0009c379006a9b8794cd0334c
Mahamat abdelkerim <mahamatabdelkerim863@gmail.com>
2022-11-07 13:12:57 +0530

    update

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,227 +1,73 @@
-// ...........BURGER MENU........... //
+document.getElementById('donate-btn').onclick = function () {
+    location.href = '../donate/index.html';
+  };
 
-const openBurger = document.querySelector('.hamburger__open')
-const closeBurger = document.querySelector('.hamburger__close')
-const shadow = document.querySelector('.shadow')
-const burgerMenu = document.querySelector('.burger__menu')
-const navigationLink = document.querySelectorAll('.navigation__link')
-const body = document.querySelector('body')
+  document.getElementById('logo-anchor').onclick = function () {
+    location.href = './index.html';
+  };
+  document.getElementById('logo-anchor2').onclick = function () {
+    location.href = './index.html';
+  };
 
-openBurger.addEventListener('click', () => {
-    burgerMenu.classList.add('burger__menu_open')
-    shadow.classList.add('shadow_open')
-    body.classList.add('scroll')
-});
-
-closeBurger.addEventListener('click', closeMenu)
-shadow.addEventListener('click', closeMenu)
-navigationLink.forEach((link) => {
-    link.addEventListener('click', closeMenu)
-});
-
-function closeMenu() {
-    burgerMenu.classList.remove('burger__menu_open')
-    shadow.classList.remove('shadow_open')
-    body.classList.remove('scroll')
-};
-
-
-// ...........TESTIMONIALS SLIDER........... //
-
-const testimonialsContainer = document.querySelector('.testimonials__container');
-const testimonialsInput = document.querySelector('.testimonials__input');
-
-testimonialsInput.addEventListener('input', () => {
-    if (document.documentElement.clientWidth > 1281) {
-        testimonialsContainer.style.transform = `translateX(-${testimonialsInput.value * 297}px)`;
-    } else {
-        testimonialsInput.setAttribute('max', '8')
-        testimonialsContainer.style.transform = `translateX(-${testimonialsInput.value * 323}px)`;
+  const catalogueImgArr = document.getElementsByClassName('grid-card');
+  for (let index = 0; index < catalogueImgArr.length; index++) {
+    catalogueImgArr[index].onmouseenter = () => {
+      catalogueImgArr[index].getElementsByTagName('img')[0].style.cssText = `
+      transform: scale(1.1);
+      transition: 1s;
+      -webkit-filter: brightness(50%);
+      -moz-filter: brightness(50%);
+      -o-filter: brightness(50%);
+      -ms-filter: brightness(50%);
+      filter: brightness(50%);`;
+      catalogueImgArr[index].getElementsByTagName('h6')[0].style.cssText = `
+        width: 312px;
+        text-align: center;
+        margin: auto;
+        transform: translate(0,-230px);
+        transition: 1s;
+        color: white;
+        font-size: 20px;
+        font-family: "Roboto-500";
+      `;
+      catalogueImgArr[index].getElementsByTagName('p')[0].style.cssText = `
+        width: 312px;
+        margin: auto;
+        text-align: center;
+        transform: translate(0,-200px);
+        transition: 1s;
+        color: white;
+        font-size: 18px;
+        font-family: "Roboto-400";
+      `;
     };
-});
-
-// ...........TESTIMONIALS POPUP........... //
-
-const feedback = document.querySelectorAll('.feedback')
-const feedbackImage = document.querySelectorAll('.feedback__image')
-const feedbackImagePopup = document.querySelector('.feedback__image_popup')
-const feedbackTitle = document.querySelectorAll('.feedback__title')
-const feedbackTitlePopup = document.querySelector('.feedback__title_popup')
-const feedbackDesc = document.querySelectorAll('.feedback__desc')
-const feedbackDescPopup = document.querySelector('.feedback__desc_popup')
-const feedbackText = document.querySelectorAll('.feedback__text')
-const feedbackTextPopup = document.querySelector('.feedback__text_popup')
-const wrapperPopup = document.querySelector('.popup__wrapper')
-const feedbackСlosePopup = document.querySelector('.feedback__close_popup')
-
-function createFeedback(element) {
-    feedbackImagePopup.src = feedbackImage[element].src
-    feedbackTitlePopup.innerHTML = feedbackTitle[element].textContent
-    feedbackDescPopup.innerHTML = feedbackDesc[element].textContent
-    feedbackTextPopup.innerHTML = feedbackText[element].textContent
-};
-
-feedback.forEach((element) => {
-    element.addEventListener('click', () => {
-        wrapperPopup.classList.add('popup__hidden')
-        body.classList.add('scroll')
-        createFeedback(element.id)
-    });
-});
-
-feedbackСlosePopup.addEventListener('click', () => {
-    wrapperPopup.classList.toggle('popup__hidden')
-    body.classList.toggle('scroll')
-});
-
-wrapperPopup.addEventListener('click', (event) => {
-    if (event.target.classList.contains('popup__hidden')) {
-        wrapperPopup.classList.toggle('popup__hidden')
-        body.classList.toggle('scroll')
+    catalogueImgArr[index].onmouseleave = () => {
+      catalogueImgArr[index].getElementsByTagName('img')[0].style.cssText = `
+      transform: scale(1);
+      transition: 1s;
+      -webkit-filter: brightness(100%);
+      -moz-filter: brightness(100%);
+      -o-filter: brightness(100%);
+      -ms-filter: brightness(100%);
+      filter: brightness(100%);`;
+      catalogueImgArr[index].getElementsByTagName('h6')[0].style.cssText = `
+        transform: translate(0,0);
+        transition: 1s;
+        color: black;
+        font-family: "Roboto-500";
+        font-size: 16px;
+      `;
+      catalogueImgArr[index].getElementsByTagName('p')[0].style.cssText = `
+        transform: translate(0,0);
+        transition: 1s;
+        color: black;
+        font-family: "Roboto-300";
+        font-size: 15px;
+      `;
     };
-});
-
-
-// ...........PETS SLIDER........... //
-
-import petsCards from "../../assets/js/const.js";
-
-const arrowPrev = document.querySelector('.arrow__prev')
-const arrowNext = document.querySelector('.arrow__next')
-const itemsLayouts = document.querySelectorAll('.cards__layout');
-
-let currentLayout = 0;
-let isEnabled = true;
-let countCard = 6;
-
-function createPetsCard(index) {
-    const cardImage = document.createElement('img');
-    cardImage.classList.add('card__image');
-    cardImage.src = petsCards[index].image;
-
-    const cardTitle = document.createElement('p');
-    cardTitle.classList.add('card__title');
-    cardTitle.innerHTML = petsCards[index].title;
-
-    const cardDescription = document.createElement('p');
-    cardDescription.classList.add('card__description');
-    cardDescription.innerHTML = petsCards[index].description;
-
-    const cardContent = document.createElement('div');
-    cardContent.classList.add('card__content');
-    cardContent.append(cardTitle, cardDescription);
-
-    const cardFood = document.createElement('div');
-    cardFood.classList.add('card__icon', petsCards[index].foodIcon);
-
-    const cardContentWrapper = document.createElement('div');
-    cardContentWrapper.classList.add('card__content_wrapper');
-    cardContentWrapper.append(cardContent, cardFood);
-
-    const petsCard = document.createElement('div');
-    petsCard.classList.add('card');
-    petsCard.append(cardImage, cardContentWrapper);
-
-    return petsCard;
-};
-
-function updateCountCard() {
-    if (document.documentElement.clientWidth > 980) {
-        countCard = 6;
-    } else {
-        countCard = 4;
-    };
-};
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * petsCards.length);
-};
-
-function getRandomArray(countCard) {
-    let randomArray = [];
-    let randomNum = getRandomNumber();
-    for (let i = 0; i < countCard; i++) {
-        while (randomArray.includes(randomNum)) {
-            randomNum = getRandomNumber();
-        };
-        randomArray.push(randomNum);
-    };
-    return randomArray;
-};
-
-function addCardsLayouts(itemsLayouts) {
-    updateCountCard();
-    for (let cardsLayout of itemsLayouts) {
-        cardsLayout.innerHTML = '';
-    };
-    for (let cardsLayout of itemsLayouts) {
-        // let randomArray = getRandomArray(countCard); 
-        for (let i = 0; i < countCard; i++) {
-            // cardsLayout.append(createPetsCard(randomArray[i]));
-            cardsLayout.append(createPetsCard([i]));
-        };
-    };
-};
-
-addCardsLayouts(itemsLayouts);
-
-window.addEventListener("resize", function () {
-    addCardsLayouts(itemsLayouts);
-});
-
-function getCardsLayoutsNotActive() {
-    const layoutsNotActive = [];
-    for (let cardsLayout of itemsLayouts) {
-        if (!cardsLayout.classList.contains('active')) {
-            layoutsNotActive.push(cardsLayout);
-        };
-    };
-    return layoutsNotActive;
-};
-
-function changeCurrentLayout(n) {
-    currentLayout = (n + itemsLayouts.length) % itemsLayouts.length;
-};
-
-function hideLayout(direction) {
-    isEnabled = false;
-    itemsLayouts[currentLayout].classList.add(direction);
-    itemsLayouts[currentLayout].addEventListener('animationend', function () {
-        this.classList.remove('active', direction);
-    });
-};
-
-function showLayout(direction) {
-    itemsLayouts[currentLayout].classList.add('next', direction);
-    itemsLayouts[currentLayout].addEventListener('animationend', function () {
-        this.classList.remove('next', direction);
-        this.classList.add('active');
-        isEnabled = true;
-    });
-};
-
-function nextLayout(n) {
-    hideLayout('to-left');
-    changeCurrentLayout(n + 1);
-    showLayout('from-right');
-};
-
-function previousLayout(n) {
-    hideLayout('to-right');
-    changeCurrentLayout(n - 1);
-    showLayout('from-left');
-};
-
-arrowNext.addEventListener('click', function () {
-    if (isEnabled) {
-        nextLayout(currentLayout);
-        addCardsLayouts(getCardsLayoutsNotActive());
-    };
-});
-
-arrowPrev.addEventListener('click', function () {
-    if (isEnabled) {
-        previousLayout(currentLayout);
-        addCardsLayouts(getCardsLayoutsNotActive());
-    };
-});
+    // console.log(catalogueImgArr[index].getElementsByTagName('p')[0].innerText);
+    // console.log(catalogueImgArr[index].getElementsByTagName('h6')[0].innerText);
+    // console.log(catalogueImgArr[index].getElementsByTagName('p')[0].innerText);
+    // getElementsByTagName('a');
+  }
+  // catalogueImgArr.forEach((element) => {});
