07cfb35aed6d95026f3f95886bb5818e71903d43
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2022-11-07 15:03:08 +0530

    Add files via upload

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,6 @@
+document.getElementById('logo-anchor').onclick = function () {
+  location.href = '../main/index.html';
+};
+document.getElementById('logo-anchor2').onclick = function () {
+  location.href = '../main/index.html';
+};
